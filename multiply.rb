4df022cc9fd370b3b16126005737ae10e31e13cd
kwin555 <ktn555@att.net>
2018-02-07 19:59:39 +0000

    test to make sure inputs are of class Integer

--- multiply.rb
+++ multiply.rb
@@ -1,3 +1,7 @@
 def multiply(number1, number2)
-  number1 * number2
+  if number1.is_a?(Integer) && number2.is_a?(Integer)
+    number1 * number2
+  else
+    puts 'Give inputs that are integers'
+  end
 end
